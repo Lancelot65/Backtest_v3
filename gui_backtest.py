29a812723140eab4734fb54aa8d92f0caa1f8a76
Lancelot <122176809+Lancelot65@users.noreply.github.com>
2023-08-25 23:36:35 +0200

    v2.1

--- gui_backtest.py
+++ gui_backtest.py
@@ -1,13 +1,12 @@
-from tkinter import Tk, PhotoImage, Frame, Label, Button, ttk, StringVar, Toplevel, Spinbox, Checkbutton, IntVar, Entry, messagebox
+from tkinter import Tk, PhotoImage, Frame, Label, Button, ttk, StringVar, Toplevel, Spinbox, Checkbutton, IntVar, Entry, messagebox, BooleanVar
 import sys
 from ccxt import binance
 sys.path.append('../trad/Ohlcvplus')
 from ohlcv import OhlcvPlus
 from mplfinance.original_flavor import candlestick_ohlc
+from matplotlib.dates import date2num, DateFormatter
 from datetime import datetime
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
 
 class gui(Tk):
@@ -15,8 +14,9 @@ class gui(Tk):
         super().__init__()
 
         self.color = StringVar(value='lavender')
+        self.data = None
 
-        self.calcul_indicateur = []
+        self.indicateur = {}
 
         self.option_add('*Font', 'Times 10')
         #self.configure(bg=self.color.get())
@@ -32,25 +32,20 @@ class gui(Tk):
         self.frame_ind.grid(row=1, column=0)
         self.plus_option(self.frame_ind)
 
-        Button(self, text='charger\ntableau', command=self.boutton_charger_press).grid(row=3, column=0, pady=5)
-
         self.mainloop()
     
     def reglage_simple(self, frame):
         Label(frame, text='Paramètre graphique', font=("Times 15")).grid(row=0, column=1, ipady=10)
         Label(frame, text='exchange').grid(row=1, column=0)
-        Label(frame, text='type de shéma').grid(row=1, column=1)
         Label(frame, text='timeframe').grid(row=1, column=2)
+        Button(frame, text='load data', command=lambda: self.charger_data(True)).grid(row=2, column=1, rowspan=2, ipady=10)
+        
 
         self.exchange = ttk.Combobox(frame, values=["BTC/USDT", "EUR/USDT"], state="readonly")
         self.exchange.current(0)
         self.exchange.grid(row=2, column=0, padx=5)
 
-        self.type_graphique = ttk.Combobox(frame, values=["ligne", "bougie"], state="readonly")
-        self.type_graphique.current(1)
-        self.type_graphique.grid(row=2, column=1, padx=5)
-
-        self.timeframe = ttk.Combobox(frame, values=["1m", "5m","30m","1h", "1w"], state="readonly")
+        self.timeframe = ttk.Combobox(frame, values=["1m", "5m","30m","1h", "1d", "1w"], state="readonly")
         self.timeframe.current(2)
         self.timeframe.grid(row=2, column=2, padx=5)
 
@@ -89,52 +84,62 @@ class gui(Tk):
         self.jour_fin = StringVar(value=datetime.now().day)
         Spinbox(frame2, textvariable=self.jour_fin, from_=1, to=31, width=2).grid(row=0, column=5)
         Label(frame2, text=' ',  width=1).grid(row=0, column=6)
-
     
     def charger_graphique(self):
-        self.graphique = Toplevel(self)
 
         close = self.data.close
         open = self.data.open
         high = self.data.high
         low = self.data.low
 
-        fig = Figure(figsize=(6, 4), dpi=100)
-        self.ax = fig.add_subplot(111)
-        if self.type_graphique.get() == 'bougie':
-            candlestick_ohlc(self.ax, self.data.values, colorup='g', colordown='r', width=0.0000002, alpha=0.9)
-        else:
-            self.ax.plot(self.data.timestamp, self.data.close)
+        fig, ax = plt.subplots()
+        self.data.timestamp = pd.to_datetime(self.data.timestamp, unit='ms').apply(date2num)
+                   
+        candlestick_ohlc(ax, self.data.values, colorup='g', colordown='r', width=0.0000002, alpha=0.9)
+        ax.xaxis.set_major_formatter(DateFormatter('%d-%m-%Y'))
+        fig.autofmt_xdate()
+        fig.tight_layout()
 
-        canvas = FigureCanvasTkAgg(fig, master=self.graphique)
-        canvas_widget = canvas.get_tk_widget()
-        canvas_widget.pack()
+        for ind in self.indicateur:
+            if self.indicateur[ind][1].get() == True:
+                ax.plot(self.data.timestamp, eval(self.indicateur[ind][0]), label=ind)
+        
 
-        for ind in self.calcul_indicateur:
-            self.ax.plot(self.data.timestamp, eval(ind))
+        self.protocol("WM_DELETE_WINDOW", self.fermer_fenetre)
 
-    def charger_data(self):
-        date_debut = self.annee_debut.get() + '-' + self.mois_debut.get() + '-' + self.jour_debut.get() + ' 00:00:00'
-        date_fin = self.annee_fin.get() + '-' + self.mois_fin.get() + '-' + self.jour_fin.get() + ' 00:00:00'
+        ax.legend()
+        plt.show()
+
+    def charger_data(self, forcer=False):
+        if self.data is None or forcer == True:
+            date_debut = self.annee_debut.get() + '-' + self.mois_debut.get() + '-' + self.jour_debut.get() + ' 00:00:00'
+            date_fin = self.annee_fin.get() + '-' + self.mois_fin.get() + '-' + self.jour_fin.get() + ' 00:00:00'
 
 
-        ohlcvp = OhlcvPlus(binance())
-        self.data = ohlcvp.load(market=self.exchange.get(), timeframe=self.timeframe.get(), since=date_debut, limit=date_fin, update=True, verbose=True, workers=100)
+            ohlcvp = OhlcvPlus(binance())
+            self.data = ohlcvp.load(market=self.exchange.get(), timeframe=self.timeframe.get(), since=date_debut, limit=date_fin)
 
     def boutton_charger_press(self):
         self.charger_data()
         self.charger_graphique()
     
     def plus_option(self, frame):
-        Label(frame, text='Reglage indicateurs', font=("Times 15")).grid(row=0, column=0, columnspan=2, ipady=10)
-        Label(frame, text='rentrer vos formules', font=("Times 10")).grid(row=1, column=0, padx=3, columnspan=2)
-        Label(frame, text='*vous avez accés a ces 5 variable :\nclose, open, low, high et volume',  font=("Times 8")).grid(row=3, column=0,  padx=3, sticky='w')
+        Label(frame, text='Reglage indicateurs', font=("Times 15")).grid(row=0, column=0, columnspan=2, ipady=10, sticky='e')
+        Label(frame, text='rentrer vos formules', font=("Times 10")).grid(row=1, column=0, columnspan=2, ipady=10, sticky='e')
 
         self.operation = StringVar()
         Entry(frame, textvariable=self.operation).grid(row=2, column=0, sticky='w')
 
-        Button(frame, text='check', command=self.validate).grid(row=2, column=0, sticky='e', padx=5)
-        Button(frame, text='reinitialiser', command=self.reinitialiser).grid(row=2, column=1)
+        self.name = StringVar()
+        Entry(frame, textvariable=self.name).grid(row=3, column=0, sticky='w')
+
+        Button(frame, text='check', command=self.validate).grid(row=2, column=1, sticky='w', padx=5)
+        Button(frame, text='reinitialiser', command=self.reinitialiser).grid(row=3, column=1, sticky='w', padx=5) 
+
+        Button(frame, text='charger\ntableau', command=self.boutton_charger_press).grid(row=4, column=0, pady=5)
+
+        self.frame_ind = Frame(frame)
+        self.frame_ind.grid(row=2, column=2)
     
     def validate(self):
 
@@ -147,16 +152,42 @@ class gui(Tk):
         
         try:
             eval(self.operation.get())
-            print('test_pas')
-            self.calcul_indicateur.append(self.operation.get())
-            self.operation.set('')
-        except:
-            print('error')
-            messagebox.showerror(title='error', message='error')
+            
+            if self.name.get() in self.indicateur and messagebox.askquestion('nom existant', 'Etes vous sur de modifier le calcul associe a ce nom?'):
+                self.indicateur[self.name.get()][2].destroy()
 
+                checklist_var = BooleanVar(value=True)
+                checklist = Checkbutton(self.frame_ind, text=self.name.get(), variable=checklist_var)
+                checklist.grid(row=len(self.indicateur) + 1, column=3)
+
+                
+                self.indicateur[self.name.get()] = [self.operation.get(), checklist_var, checklist]
+                
+                self.operation.set('')
+                self.name.set('')
+                print(self.indicateur)
+            else:
+                checklist_var = BooleanVar(value=True)
+                checklist = Checkbutton(self.frame_ind, text=self.name.get(), variable=checklist_var)
+                checklist.grid(row=len(self.indicateur), column=3)
+
+                
+                self.indicateur[self.name.get()] = [self.operation.get(), checklist_var, checklist]
+                
+                self.operation.set('')
+                self.name.set('')
+                print(self.indicateur)
+        except:
+             messagebox.showerror(title='error', message='error')
 
     def reinitialiser(self):
-        self.calcul_indicateur = []
+        self.indicateur = {}
+        for button in self.indicateur:
+            button[2].destroy()
+    
+    def fermer_fenetre(self):
+        plt.close()
+        self.destroy()
 
         
 gui()
--- test.py
+++ test.py
@@ -1,35 +1,20 @@
-from tkinter import Tk, Button, Label, Checkbutton, IntVar, ttk, Entry ,StringVar
-import pandas as pd
-
-root = Tk()
-
-operation = StringVar()
-liste = []
-
-df = pd.DataFrame({
-    'close' : [1, 2, 3, 4, 5],
-    'open' : [5, 4, 3, 2, 1]
-})
-
-def validate():
-    global operation
-    liste.append(operation.get())
-    operation.set('')
-
-def fin():
-    global liste, df
-    close = df.close
-    open = df.open
-    for calcul in liste:
-        print(eval(calcul))
-
-def reinitialiser():
-    global liste
-    liste = []
-
-Entry(root, textvariable=operation).grid(row=0, column=0, columnspan=2 ,ipadx=5, ipady=5)
-Button(root, command=validate).grid(row=0 ,column=2 ,ipadx=5, ipady=5)
-Button(root, text='fin', command=fin).grid(row=1, column=1)
-Button(root, text='reinitialiser', command=reinitialiser).grid(row=2, column=1)
-
-root.mainloop()
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+ 
+x = np.linspace(0, 3, 300)
+y = np.sin(5 * np.pi * x)
+ 
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+l, = ax.plot(x, y)
+ 
+def onChange(value):
+    l.set_ydata(np.sin(value * np.pi * x))
+    fig.canvas.draw_idle()
+ 
+slideraxis = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+slider = Slider(slideraxis, label='Frequency [Hz]',
+                valmin=0, valmax=10, valinit=5)
+slider.on_changed(onChange)
+plt.show()
